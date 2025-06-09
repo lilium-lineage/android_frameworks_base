eb2a8291b4e22b9c59a885ee9464b4840ca855dd
HeroBuxx <me@herobuxx.me>
2025-06-10 01:42:08 +0800

    core: Import DevicePropsSpoofing from Halcyon
    Imported from fe390a3bdba29f53d191bf60b3098c3c76f1a327

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -75,6 +75,7 @@ import java.util.Objects;
 import java.util.StringJoiner;
 import java.util.concurrent.TimeoutException;
 
+import com.android.internal.util.halcyon.DevicePropsSpoofing;
 /**
  * Base class for implementing application instrumentation code.  When running
  * with instrumentation turned on, this class will be instantiated for you
@@ -1356,6 +1357,7 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
+        DevicePropsSpoofing.setProps(context);
         return app;
     }
     
@@ -1373,6 +1375,7 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
+        DevicePropsSpoofing.setProps(context);
         return app;
     }
 
--- keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
+++ keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -178,6 +178,7 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
+
         KeyEntryResponse response = getKeyMetadata(alias);
 
         if (response == null || response.metadata.certificate == null) {
